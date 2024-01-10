910df3de8123506e258a6a1fef44c6d0286dfba9
Rufus97 <manuelgomez@outlook.it>
2024-01-10 22:46:52 +0100

    changed loopGioco logic

--- src/LoopGioco.java
+++ src/LoopGioco.java
@@ -6,29 +6,52 @@ public class LoopGioco {
         Personaggio personaggio = new Personaggio();
         // Creazione dell'inventario
         boolean loopGioco = true;
-        if (personaggio.getPosizione() == 0) {
-            while (loopGioco) {
-                System.out.println("Ti trovi in: " + personaggio.getPosizione());
-                System.out.println("Cosa vuoi fare? (1. Prova , 2. Test, Scrivi Quit per uscire)");
 
-                //Scelta movimento
-                String scelta = In.scanner.nextLine();
-                switch (scelta.toLowerCase()) {
-                    case "1":
-                        personaggio.setPosizione(1);
-                        break;
-                    case "2":
-                        personaggio.setPosizione(2);
-                        break;
-                    case "quit":
-                        // Azione per uscire dal gioco
-                        loopGioco = false;
-                        break;
-                    default:
-                        System.out.println("Scelta non valida. Riprova.");
-                }
+        //Start loop di gioco
+
+
+        while (loopGioco) {
+            System.out.println("Ti trovi in: " + personaggio.getPosizione());
+
+
+
+            // Controlla la posizione del personaggio e esegui azioni specifiche della stanza
+            switch (personaggio.getPosizione()) {
+                case 0:
+                    //Switch per definire la nuova posizione
+                    System.out.println("Cosa vuoi fare? (1. Prova , 2. Test, Scrivi Quit per uscire)");
+                    String scelta = In.scanner.nextLine();
+                    switch (scelta.toLowerCase()) {
+                        case "1":
+                            personaggio.setPosizione(1);
+                            break;
+                        case "2":
+                            personaggio.setPosizione(2);
+                            break;
+                        case "quit":
+                            // Azione per uscire dal gioco
+                            loopGioco = false;
+                            break;
+                        default:
+                            System.out.println("Scelta non valida. Riprova.");
+                    }
+                    break;
+                case 1:
+                    System.out.println("Sito n°1 ancora in costruzione! ritornare più tardi");
+                    loopGioco = false;
+                    break;
+                case 2:
+                    System.out.println("Sito n°2 ancora in costruzione! ritornare più tardi");
+                    loopGioco = false;
+                    break;
+                default:
+                    System.out.println("Posizione non gestita.");
+                    loopGioco = false;  // Esci dal loop in caso di posizione non gestita
+
             }
-        }else {loopGioco = false;}
+        }
+
+
         // Loop di gioco
         System.out.println("Grazie per aver giocato!");
     }
--- src/Stanze/PuntoSNAI.java
+++ src/Stanze/PuntoSNAI.java
@@ -4,5 +4,6 @@ public class PuntoSNAI extends Stanza {
 
     public PuntoSNAI(String nome, String descrizione) {
         super(nome, descrizione);
+        System.out.println("Sei dentro il punto snai");
     }
 }
