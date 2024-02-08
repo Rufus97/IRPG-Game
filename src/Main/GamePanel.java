80044fc7c5620c3fb33acf3a3bacc8e677df24a8
Rufus97 <manuelgomez@outlook.it>
2024-02-08 22:34:59 +0100

    added slot

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -4,54 +4,53 @@ import Input.In;
 import Player.Inventario;
 import Player.Personaggio;
 
-
 public class GamePanel {
 
-    public static final Personaggio giocatore = new Personaggio();
-    public static final Inventario inventario = new Inventario();       //far vedere
+	public static final Personaggio giocatore = new Personaggio();
+	public static final Inventario inventario = new Inventario(); // far vedere
 
-    //costruttore
-    public GamePanel(){
-    }
+	// costruttore
+	public GamePanel() {
+	}
 
-        public void startGame(){
-        
-            int scelta;
+	public void startGame() {
 
-            //Loop menu
-            do{
-                System.out.println("Cosa vuoi fare?");
-                System.out.println("1. Vai a...");
-                System.out.println("2. Apri Inventario...");
-                System.out.println("0. Esci dal gioco");
-                scelta = In.scanner.nextInt();
-                GamePanel.clearScreen();
-                    switch (scelta) {
-                        case 1: 
-                        Menu.VaiA();
-                        break;
+		int scelta;
 
-                        case 2:
-                        System.out.println("Nell inventario hai:");
-                        System.out.println(GamePanel.inventario.toString());
-                    
-                        break;
+		// Loop menu
+		do {
+			System.out.println("Cosa vuoi fare?");
+			System.out.println("1. Vai a...");
+			System.out.println("2. Apri Inventario...");
+			System.out.println("0. Esci dal gioco");
+			scelta = In.scanner.nextInt();
+			GamePanel.clearScreen();
+			switch (scelta) {
+			case 1:
+				Menu.VaiA();
+				break;
 
-                        case 3:
-                        System.out.println("Exit Game");
-                        break;
-        
-                    default:
-                        System.out.println("Scelta errata");
-                        break;   
-                    }
-                    
-        }while( scelta !=0 );
-    }
+			case 2:
+				System.out.println("Nell inventario hai:");
+				System.out.println(GamePanel.inventario.toString());
 
-    //METODO CANCELLA SCHERMO
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
+				break;
+
+			case 3:
+				System.out.println("Exit Game");
+				break;
+
+			default:
+				System.out.println("Scelta errata");
+				break;
+			}
+
+		} while (scelta != 0);
+	}
+
+	// METODO CANCELLA SCHERMO
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
 }
--- src/Player/Oggetto.java
+++ src/Player/Oggetto.java
@@ -1,34 +1,33 @@
 package Player;
 // Suppongo sia necessaria anche una superclasse "Oggetto"
 
-    public class Oggetto {
-        private String nome;
-        private int quantita;
+public class Oggetto {
+	private String nome;
+	private int quantita;
 
-        public Oggetto(String nome, int quantita) {
-            this.nome = nome;
-            this.quantita = quantita;
-        }
+	public Oggetto(String nome, int quantita) {
+		this.nome = nome;
+		this.quantita = quantita;
+	}
 
-        public String getNome() {
-            return nome;
-        }
+	public String getNome() {
+		return nome;
+	}
 
-        public int getQuantita() {
-            return quantita;
-        }
+	public int getQuantita() {
+		return quantita;
+	}
 
-        public void setNome(String nome) {
-            this.nome = nome;
-        }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-        public void setQuantita(int quantita) {
-            this.quantita = quantita;
-        }
+	public void setQuantita(int quantita) {
+		this.quantita = quantita;
+	}
 
-        @Override
-        public String toString() {
-            return "\n" + nome + " quantita = " + quantita ;
-        }
-    }
-
+	@Override
+	public String toString() {
+		return "\n" + nome + " quantita = " + quantita;
+	}
+}
