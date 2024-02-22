a5ca69fa29cfd5f9ebbc06da866a85367dc1738a
Fabio Fornari <fubu90.ff@gmail.com>
2024-02-22 12:47:46 +0100

    momdifica simon69

--- IRPG-Game/src/Main/Main.java
+++
@@ -1,8 +0,0 @@
-package Main;
-public class Main {
-    public static void main(String[] args) throws Exception {
-        GamePanel gamePanel = new GamePanel();
-
-        gamePanel.startGame();
-    }
-}
--- IRPG-Game/src/Stanze/Tribunale.java
+++
@@ -1,4 +0,0 @@
-package Stanze;
-
-public class Tribunale {
-}
