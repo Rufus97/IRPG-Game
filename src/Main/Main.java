5dac38fbfbe4ddee64ad872c43cf35fa108a1f3d
Simona Labib <152871220+SimonaLabib@users.noreply.github.com>
2024-03-07 13:52:47 +0100

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -14,8 +14,8 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-	 GamePanel start = new GamePanel();
-	 start.startGame();
+	 GamePanel game = new GamePanel();
+	 game.startGame();
 
 
 	}
