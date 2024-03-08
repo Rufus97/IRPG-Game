94243aeb441a162797507c8fd165cfbe422be8f1
Rufus97 <manuelgomez@outlook.it>
2024-03-08 09:00:47 +0100

    Horse fix

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,10 @@
 package Main;
 
+import Stanze.puntoSnai.HorseP;
 
 public class Main {
 	public static void main(String[] args) {
 	 GamePanel game = new GamePanel();
 	 game.startGame();
-
 	}
 }
