92f79a0390d5e727261373a874a4d9f8b3aa459e
Rufus97 <manuelgomez@outlook.it>
2024-03-14 02:15:18 +0100

    fix cavalli

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,9 +4,8 @@ import Stanze.puntoSnai.HorseP;
 
 public class Main {
 	public static void main(String[] args) {
-		HorseP cavallo = new HorseP();
-		cavallo.runHorses();
-//	 GamePanel game = new GamePanel();
-//	 game.startGame();
+
+	 GamePanel game = new GamePanel();
+	 game.startGame();
 	}
 }
